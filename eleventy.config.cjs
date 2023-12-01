dc9cdf4298d77d53f162d7f702622ba027fb356c
Dwan <me@dwaan.com>
2023-12-01 15:28:17 +0200

    Modularized template

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -8,12 +8,17 @@ module.exports = function (eleventyConfig) {
         watch: ["sites/css/*", "sites/js/*"]
     });
 
+    eleventyConfig.addShortcode("svg", function (icon, size, view, alt) {
+        return `<svg width="${size}" height="${size}" viewBox="0 0 ${view} ${view}" aria-label="${alt}"> <use href="/img/icons/${icon}}" /></svg>`;
+    });
+
     return {
         dir: {
             input: "src",
             output: "sites",
             includes: "_includes",
-            layouts: "_layouts"
+            layouts: "_layouts",
+            shortcode: "_shortcode"
         },
         passthroughFileCopy: true
     }
